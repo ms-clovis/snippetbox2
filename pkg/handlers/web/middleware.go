f659b8c9a6c7f940231922361c968da0d00e571a
mike <ms.clovis@verizon.net>
2020-01-09 13:13:49 -0500

    Added CSRF token (alice) setter

--- pkg/handlers/web/middleware.go
+++ pkg/handlers/web/middleware.go
@@ -3,9 +3,21 @@ package web
 import (
 	"fmt"
 	slog "github.com/go-eden/slf4go"
+	"github.com/justinas/nosurf"
 	"net/http"
 	"runtime/debug"
 )
+
+func CSRFTokenSetter(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+
+	return csrfHandler
+}
 
 func SecureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
